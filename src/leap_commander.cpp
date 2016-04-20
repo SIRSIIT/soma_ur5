a27b4bc33c0a9d9be579c6de45565e56be573193
Joao Bimbo <joao.bimbo@iit.it>
2016-04-20 18:03:33 +0200

    hand angles and opening done :)

--- src/leap_commander.cpp
+++ src/leap_commander.cpp
@@ -6,70 +6,89 @@
 
 class LeapCom{
 public:
-LeapCom(){
-    this->nh=new ros::NodeHandle();
+    LeapCom(){
+        this->nh=new ros::NodeHandle();
 
-    pub_robot_com=nh->advertise<geometry_msgs::PoseStamped>("/goal_pose",5);
-    sub_pose= nh->subscribe("ee_pose", 1000, &LeapCom::robot_pose_callback, this);
-    sub_leap= nh->subscribe("leapmotion/data", 1000, &LeapCom::leap_callback, this);
+        pub_robot_com=nh->advertise<geometry_msgs::PoseStamped>("/goal_pose",5);
+        sub_pose= nh->subscribe("ee_pose", 1000, &LeapCom::robot_pose_callback, this);
+        sub_leap= nh->subscribe("leapmotion/data", 1000, &LeapCom::leap_callback, this);
 
-}
-~LeapCom(){}
-void leap_loop(){
+    }
+    ~LeapCom(){}
+    void leap_loop(){
 
-}
+    }
 
 protected:
-ros::Publisher pub_robot_com;
-ros::Subscriber sub_pose,sub_leap;
-geometry_msgs::PoseStamped ee_pose,leap_pose;
-ros::NodeHandle *nh;
-tf2_ros::TransformBroadcaster tf_br;
+    ros::Publisher pub_robot_com;
+    ros::Subscriber sub_pose,sub_leap;
+    geometry_msgs::PoseStamped ee_pose,leap_pose;
+    ros::NodeHandle *nh;
+    tf2_ros::TransformBroadcaster tf_br;
 
-void leap_callback(const leap_motion::leapros::ConstPtr &msg){
-    leap_pose.pose.position.x=msg->palmpos.x/1000;
-    leap_pose.pose.position.y=msg->palmpos.y/1000;
-    leap_pose.pose.position.z=msg->palmpos.z/1000;
+    geometry_msgs::TransformStamped get_hand_transform(leap_motion::leapros::ConstPtr msg){
+        geometry_msgs::TransformStamped trans;
+        tf2::Quaternion q;
+        //q.setRPY(msg->ypr.x*M_PI/180,msg->ypr.y*M_PI/180,msg->ypr.z*M_PI/180);
+        //q.setRPY(msg->ypr.x*M_PI/180,0,0);
+        //q.setRPY(0,-atan2(msg->normal.x,-msg->normal.y),0);
 
-    tf2::Quaternion q;
-    //q.setRPY(msg->ypr.x*M_PI/180,msg->ypr.y*M_PI/180,msg->ypr.z*M_PI/180);
-    //q.setRPY(msg->ypr.x*M_PI/180,0,0);
-    //q.setRPY(0,-atan2(msg->normal.x,-msg->normal.y),0);
+        tf2::Vector3 c(msg->normal.x,msg->normal.y,msg->normal.z);
+        tf2::Vector3 a(msg->direction.x,msg->direction.y,msg->direction.z);
+        tf2::Vector3 b(c.cross(a));
+        tf2::Matrix3x3 M(a.x(),b.x(),c.x(),
+                         a.y(),b.y(),c.y(),
+                         a.z(),b.z(),c.z());
+        M.getRotation(q);
+        //ROS_INFO("Det: %f", M.determinant());
 
+        tf2::convert(q,trans.transform.rotation);
+        trans.header.frame_id="leap_frame";
+        trans.child_frame_id="hand_frame";
+        trans.header.stamp=ros::Time::now();
+        trans.transform.translation.x=msg->palmpos.x/1000;
+        trans.transform.translation.y=msg->palmpos.y/1000;
+        trans.transform.translation.z=msg->palmpos.z/1000;
+        return trans;
 
-
-    tf2::Vector3 c(msg->normal.x,msg->normal.y,msg->normal.z);
-    tf2::Vector3 a(msg->direction.x,msg->direction.y,msg->direction.z);
-    tf2::Vector3 b(c.cross(a));
-    tf2::Matrix3x3 M(a.x(),b.x(),c.x(),
-                     a.y(),b.y(),c.y(),
-                     a.z(),b.z(),c.z());
-    M.getRotation(q);
-    //ROS_INFO("Det: %f", M.determinant());
-
-    tf2::convert(q,leap_pose.pose.orientation);
-
-
-    leap_pose.header.stamp=ros::Time::now();
-    leap_pose.header.frame_id="leap_frame";
-
-       geometry_msgs::TransformStamped trans;
-       trans.header.frame_id="leap_frame";
-       trans.child_frame_id="hand_frame";
-       trans.transform.translation.x=leap_pose.pose.position.x;
-       trans.transform.translation.y=leap_pose.pose.position.y;
-       trans.transform.translation.z=leap_pose.pose.position.z;
-       trans.transform.rotation=leap_pose.pose.orientation;
-
-
-    tf_br.sendTransform(trans);
-    pub_robot_com.publish(leap_pose);
-}
+    }
+    double get_hand_opening(leap_motion::leapros::ConstPtr msg){
+        double opening;
+        tf2::Vector3 mf_d(msg->middle_distal.x,msg->middle_distal.y,msg->middle_distal.z);
+        tf2::Vector3 mf_t(msg->middle_tip.x,msg->middle_tip.y,msg->middle_tip.z);
+        tf2::Vector3 h_d(msg->direction.x,msg->direction.y,msg->direction.z);
+        opening=(mf_t-mf_d).angle(h_d)/M_PI;
+        return opening;
+    }
 
 
-void robot_pose_callback(const geometry_msgs::PoseStamped::ConstPtr &msg){
-    ee_pose=*msg;
-}
+
+    geometry_msgs::PoseStamped transf2pose(geometry_msgs::TransformStamped in){
+        geometry_msgs::PoseStamped pose;
+        pose.header=in.header;
+        pose.pose.position.x=in.transform.translation.x;
+        pose.pose.position.y=in.transform.translation.y;
+        pose.pose.position.z=in.transform.translation.z;
+        //tf2::convert(in.transform.translation,pose.pose.position);
+        tf2::convert(in.transform.rotation   ,pose.pose.orientation);
+        return pose;
+    }
+
+    void leap_callback(const leap_motion::leapros::ConstPtr &msg){
+        geometry_msgs::TransformStamped trans;
+        double opening;
+        trans=get_hand_transform(msg);
+        tf_br.sendTransform(trans);
+        leap_pose=transf2pose(trans);
+        pub_robot_com.publish(leap_pose);
+        opening=get_hand_opening(msg);
+        ROS_INFO("opening: %f",opening);
+    }
+
+
+    void robot_pose_callback(const geometry_msgs::PoseStamped::ConstPtr &msg){
+        ee_pose=*msg;
+    }
 
 };
 
